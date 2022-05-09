7f0f3d194ae12acbf080c393940a4e43c0852db0
hy-atoz <hooiyan.atoz@gmail.com>
2022-05-09 17:38:06 +0800

    Display all countries name & flag

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import './CountryDetail.css';
 
 export default function CountryDetail() {
   const [countries, setCountries] = React.useState([]);
@@ -6,28 +8,41 @@ export default function CountryDetail() {
   React.useEffect(() => {
     const url = 'https://restcountries.com/v3.1/all';
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.err(err));
+    axios.get(url).then((res) => {
+      setCountries(res.data);
+    });
   }, []);
 
-	// setCountries((originalData) => [...originalData, data])
+  // setCountries((originalData) => [...originalData, data])
 
-  function fetchData() {
-    const url = 'https://restcountries.com/v3.1/all';
+  // function fetchData() {
+  //   const url = 'https://restcountries.com/v3.1/all';
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.err(err));
-  }
-
-	console.log(countries)
+  //   fetch(url)
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data))
+  //     .catch((err) => console.err(err));
+  // }
 
   return (
     <div>
-      <button onClick={fetchData}>Fetch data</button>
+      {countries.length === 0 ? (
+        <p>Data is loading...</p>
+      ) : (
+        <>
+          <h1 className='title'>REST Countries API</h1>
+          <div className='countries-container'>
+            {countries.map((country) => {
+              return (
+                <div key={country.name.common} className='country-container'>
+                  <h2>{country.name.common}</h2>
+                  <img src={country.flags.png} alt={country.name.common} />
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
